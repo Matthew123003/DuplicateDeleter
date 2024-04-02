e279a2761b1faf79b8fbc8334fbf9c3b7418890b
Matt Kramer <matthew123003@gmail.com>
2024-04-02 10:08:58 -0400

    Finished

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,5 +1,10 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -11,13 +16,35 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for(Integer num : array){
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
 
-        return new Integer[0];
+        List<Integer> results = new ArrayList<>();
+
+        for(Integer num : array){
+            if(freqMap.get(num) < maxNumberOfDuplications){
+                results.add(num);
+            }
+        }
+        return results.toArray(new Integer[0]);
     }
 
     @Override
     public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for(Integer num : array){
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
 
-        return new Integer[0];
+        List<Integer> results = new ArrayList<>();
+
+        for(Integer num : array){
+            if(freqMap.get(num) != exactNumberOfDuplications){
+                results.add(num);
+            }
+        }
+        return results.toArray(new Integer[0]);
     }
 }
